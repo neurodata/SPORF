9477131ad238cdd7d6a5a6b4f67c9f655b93f747
Tyler Tomita <tmtomita87@gmail.com>
2017-09-18 17:34:45 -0400

    export StrCorr and add example

--- rerf/R/StrCorr.R
+++ rerf/R/StrCorr.R
@@ -11,6 +11,14 @@
 #' 
 #' @export
 #'
+#' @examples
+#' library(rerf)
+#' trainIdx <- c(1:40, 51:90, 101:140)
+#' X <- as.matrix(iris[,1:4])
+#' Y <- as.numeric(iris[,5])
+#' forest <- RerF(X[trainIdx, ], Y[trainIdx], num.cores = 1L)
+#' predictions <- Predict(X[-trainIdx, ], forest, num.cores = 1L, aggregate.output = FALSE)
+#' scor <- StrCorr(predictions, Y[-trainIdx])
 
 StrCorr <-
 function(Yhats, Y) {
