923fd2c5b22f7464826e0684b90cbad4848ba5cf
James Browne <jbrowne6@jhu.edu>
2017-09-29 13:32:26 -0400

    changed .travis.yml and test structure

--- Test/installPackages.R
+++
@@ -1,3 +0,0 @@
-list.of.packages <- c("Rcpp", "RcppArmadillo", "doParallel")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
