a340092f10428ab730ffb56ea15ef1e5e7565e26
j1c <jaewonc78@gmail.com>
2018-10-02 14:58:31 -0400

    Style fix

--- R/RunOOB.R
+++ R/RunOOB.R
@@ -5,22 +5,21 @@
 #' @param X an n sample by d feature matrix (preferable) or data frame which was used to train the provided forest.
 #' @param tree a tree from a forest returned by RerF.
 #'
-#' @return predictions prediction matrix used by OOBPredict
+#' @return out prediction matrix used by OOBPredict
 #'
 
-RunOOB <-
-    function(X, tree) {
-        n.all <- nrow(X)
-        num.classes <- ncol(tree$ClassProb)
-
-        # Get OOB samples
-        X <- X[tree$ind, , drop = FALSE]
-
-        # Predict OOB samples
-        predictions <- RunPredict(X, tree)
-
-        # Create a matrix for all of samples
-        out <- matrix(0, nrow = n.all, ncol = num.classes)
-        out[tree$ind, ] <- predictions
-        return(out)
-    }
+RunOOB <- function(X, tree) {
+  n.all <- nrow(X)
+  num.classes <- ncol(tree$ClassProb)
+  
+  # Get OOB samples
+  X <- X[tree$ind, , drop = FALSE]
+  
+  # Predict OOB samples
+  predictions <- RunPredict(X, tree)
+  
+  # Create a matrix for all of samples
+  out <- matrix(0, nrow = n.all, ncol = num.classes)
+  out[tree$ind, ] <- predictions
+  return(out)
+}
