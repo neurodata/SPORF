e6d2505afbe464d91de21ce9aa916ceabb4bd013
j1c <jaewonc78@gmail.com>
2018-10-02 17:01:39 -0400

    Add testthat tests for predictions

---
+++ tests/testthat/test-predictions.R
@@ -0,0 +1,94 @@
+context("Predictions and OOB Predictions")
+library(rerf)
+
+set.seed(123456)
+
+# Get iris
+X <- as.matrix(iris[, 1:4])
+Y <- iris[[5L]]
+
+# Set training and testing data
+trainIdx <- c(1:30, 51:80, 101:120)
+X.train <- X[trainIdx, ]
+Y.train <- Y[trainIdx]
+X.test <- X[-trainIdx, ]
+Y.test <- Y[-trainIdx]
+
+test_that("OOB predictions fails when OOB indices not stored", {
+  forest <- RerF(X, Y, seed = 1L, num.cores = 1L)
+  oob.predictions <- expect_error(Predict(X, forest, OOB = TRUE, store.oob = FALSE))
+})
+
+test_that("Predictions fail when training data not given for rank.transform", 
+  {
+    forest <- RerF(X, Y, seed = 1L, num.cores = 1L, store.oob = TRUE, 
+      min.parent = 1, max.depth = 0, rank.transform = TRUE)
+    expect_error(oob.predictions <- Predict(X, forest, OOB = TRUE))
+  })
+
+test_that("Test fails when input is not matrix", {
+  forest <- RerF(X, Y, seed = 1L, num.cores = 1L, store.oob = TRUE, min.parent = 1, 
+    max.depth = 0, rank.transform = TRUE)
+  bad.input <- "bad"
+  expect_error(Predict(bad.input, forest, OOB = FALSE))
+})
+
+test_that("Iris OOB Predictions", {
+  # Build as large of trees as possible
+  forest <- RerF(X, Y, seed = 1L, num.cores = 1L, store.oob = TRUE, min.parent = 1, 
+    max.depth = 0)
+  oob.predictions <- Predict(X, forest, OOB = TRUE)
+  accuracy <- mean(Y == oob.predictions)
+  expect_equal(accuracy, 143/150)
+  
+  # Limit depth of trees
+  forest <- RerF(X, Y, seed = 1L, num.cores = 1L, store.oob = TRUE, min.parent = 1, 
+    max.depth = 2)
+  oob.predictions <- Predict(X, forest, OOB = TRUE)
+  accuracy <- mean(Y == oob.predictions)
+  expect_equal(accuracy, 133/150)
+})
+
+test_that("Iris Predictions", {
+  # Build as large of trees as possible
+  forest <- RerF(X.train, Y.train, seed = 3L, num.cores = 1L, min.parent = 1, 
+    max.depth = 0)
+  predictions <- Predict(X.test, forest, OOB = FALSE)
+  accuracy <- mean(Y.test == predictions)
+  expect_equal(accuracy, 64/70)
+  
+  # Limit depth of trees
+  forest <- RerF(X.train, Y.train, seed = 3L, num.cores = 1L, min.parent = 1, 
+    max.depth = 3L)
+  predictions <- Predict(X.test, forest, OOB = FALSE, num.cores = 1L)
+  accuracy <- mean(Y.test == predictions)
+  expect_equal(accuracy, 63/70)
+})
+
+test_that("Output probabilities should equal 1", {
+  forest <- RerF(X.train, Y.train, seed = 4L, num.cores = 1L, store.oob = TRUE, 
+    min.parent = 1, max.depth = 3)
+  # Test data predictions
+  predictions <- Predict(X.test, forest, OOB = FALSE, output.scores = TRUE)
+  nrows <- nrow(predictions)
+  expect_equal(rep(1, nrows), rowSums(predictions))
+  
+  # OOB data predictions
+  oob.predictions <- Predict(X.train, forest, OOB = TRUE, output.scores = TRUE)
+  nrows <- nrow(oob.predictions)
+  expect_equal(rep(1, nrows), rowSums(oob.predictions))
+})
+
+test_that("Not aggregate output, probabilities should still equal 1", {
+  forest <- RerF(X.train, Y.train, seed = 5L, num.cores = 1L, store.oob = TRUE, 
+    min.parent = 1, max.depth = 3)
+  
+  predictions <- Predict(X.test, forest, OOB = FALSE, output.scores = TRUE, 
+    aggregate.output = FALSE)
+  trees <- length(predictions)
+  for (i in 1:trees) {
+    tree.predictions <- predictions[[i]]
+    nrows <- nrow(tree.predictions)
+    expect_equal(rep(1, nrows), rowSums(tree.predictions))
+  }
+})
