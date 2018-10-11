e8fc23e165da90af3edf877405ac103296999f23
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-10-11 12:57:18 -0400

    cran-itized.

--- .#build.R
+++ .#build.R
@@ -1,7 +1,11 @@
 #!/usr/local/bin/Rscript
 require(devtools)
 devtools::document('R')
-devtools::check(".")
+#devtools::check(".", args = c("--no-examples", "--no-tests"))
+devtools::build(".", args = c("--resave-data"))
+devtools::check(".", args = c("--as-cran"))
 devtools::install(".")
+
+
 
 
