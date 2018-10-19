765672f2ae60e549c4715f5b1265b6d75fce103b
Ben Falk <falk.ben@gmail.com>
2018-10-19 16:31:57 -0400

    outlining tests to be written for travistest conversion

---
+++ tests/testthat/test-general.R
@@ -0,0 +1,14 @@
+# run each of the following tests over a number of different data samples
+
+
+# test for num of trees generated == # requested
+
+
+# test to make sure that a node with 0 samples does not exist
+
+
+# test to make sure p values are approximately .001
+
+
+# test of run times
+
