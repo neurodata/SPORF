e4d22df648851baa1bf7f32422449722a0420167
Ben Falk <falk.ben@gmail.com>
2018-10-19 13:12:49 -0400

    update build scripts to make the RcppExports.cpp file (#69)

--- .#build.sh
+++ .#build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # Rscript -e "install.packages('devtools', repos = 'http://cran.us.r-project.org')"
+# Rscript -e "install.packages('roxygen2', repos = 'http://cran.us.r-project.org')"
 Rscript -e "devtools::document('R')"
 
+Rscript -e "devtools::Rcpp::compileAttributes()"
 R CMD build --resave-data .
 
 # Rscript -e "install.packages('dummies', repos = 'http://cran.us.r-project.org')"
 # Rscript -e "install.packages('Rcpp', repos = 'http://cran.us.r-project.org')"
 # Rscript -e "install.packages('RcppArmadillo', repos = 'http://cran.us.r-project.org')"
 # Rscript -e "install.packages('RcppZiggurat', repos = 'http://cran.us.r-project.org')"
-# Rscript -e "install.packages('roxygen2', repos = 'http://cran.us.r-project.org')"
 # Rscript -e "install.packages('testthat', repos = 'http://cran.us.r-project.org')"
 
 R CMD check --as-cran --no-manual rerf*.tar.gz
