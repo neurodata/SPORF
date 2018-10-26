254e46939c32700f85955339d22d3b9dcd57f570
Ben Falk <falk.ben@gmail.com>
2018-10-26 12:55:40 -0400

    testing diff. num. trees requested
    removed not implemented info

--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -15,33 +15,22 @@ Y.train <- Y[trainIdx]
 X.test <- X[-trainIdx, ]
 Y.test <- Y[-trainIdx]
 
-# create a single forest to test
-numTrees = 10
-forest <- RerF(X, Y, trees = numTrees, seed = 1L, num.cores = 1L, store.oob = FALSE)
-
 test_that("Testing number of trees output is as requested.", {
-    expect_equal(length(forest$trees), numTrees)
+    numTrees = c(1, 10, 50, 500)
+    for (nn in numTrees) {
+        forest <- RerF(X, Y, trees = nn, seed = 1L, num.cores = 1L, store.oob = FALSE)
+        expect_equal(length(forest$trees), nn)
+    }
 })
 
 # sum of the class probabilities should ~= 1 alternatively -- every leaf node
 # should be > 0, probability be > 0, or sum ~= 1
-test_that("Sum of the class probabilities should ~= 1", {
+test_that("Sum of the class probabilities should ~= 1.", {
+    numTrees = 10
+    forest <- RerF(X, Y, trees = numTrees, seed = 1L, num.cores = 1L, store.oob = FALSE)
     for (z in 1:length(forest$trees)) {
         for (q in 1:length(forest$trees[[z]]$ClassProb[, 1])) {
             expect_equal(sum(forest$trees[[z]]$ClassProb[q, ]), 1, tolerance = 1e-08)
         }
     }
 })
-
-##
-# Note: not implemented yet
-##
-
-# Iterate over multiple datasets and multiple run times/dataset
-
-# Compare performance (timings & accuracy) between prior "release" and current "tested" version 
-# Using dev mode on/off
-# Record timed tests & accuracy for each version
-# Compare the distributions between the different versions to ensure that they are from the "same" distribution 
-# (wilcox test p value >= .001)
-
