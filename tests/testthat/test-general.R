649cee01c268b8f1f86d3d6fb83ee26faeb88554
Ben Falk <falk.ben@gmail.com>
2018-11-01 12:33:34 -0400

    indent 2 spaces

--- tests/testthat/test-general.R
+++ tests/testthat/test-general.R
@@ -16,21 +16,21 @@ X.test <- X[-trainIdx, ]
 Y.test <- Y[-trainIdx]
 
 test_that("Testing number of trees output is as requested.", {
-    numTrees = c(1, 10, 50, 500)
-    for (nn in numTrees) {
-        forest <- RerF(X, Y, trees = nn, seed = 1L, num.cores = 1L, store.oob = FALSE)
-        expect_equal(length(forest$trees), nn)
-    }
+  numTrees = c(1, 10, 50, 500)
+  for (nn in numTrees) {
+    forest <- RerF(X, Y, trees = nn, seed = 1L, num.cores = 1L, store.oob = FALSE)
+    expect_equal(length(forest$trees), nn)
+  }
 })
 
 # sum of the class probabilities should ~= 1 alternatively -- every leaf node
 # should be > 0, probability be > 0, or sum ~= 1
 test_that("Sum of the class probabilities should ~= 1.", {
-    numTrees = 10
-    forest <- RerF(X, Y, trees = numTrees, seed = 1L, num.cores = 1L, store.oob = FALSE)
-    for (z in 1:length(forest$trees)) {
-        for (q in 1:length(forest$trees[[z]]$ClassProb[, 1])) {
-            expect_equal(sum(forest$trees[[z]]$ClassProb[q, ]), 1, tolerance = 1e-08)
-        }
+  numTrees = 10
+  forest <- RerF(X, Y, trees = numTrees, seed = 1L, num.cores = 1L, store.oob = FALSE)
+  for (z in 1:length(forest$trees)) {
+    for (q in 1:length(forest$trees[[z]]$ClassProb[, 1])) {
+      expect_equal(sum(forest$trees[[z]]$ClassProb[q, ]), 1, tolerance = 1e-08)
     }
+  }
 })
--- travisTest/test-on-prior-release.R
+++ travisTest/test-on-prior-release.R
@@ -14,10 +14,10 @@ cores <- c(rep(numC, 4), 25)
 numTrees <- c(rep(trees, 4), 100)
 
 path <- "travisTest/"
-trainSets <- c("Orthant_train.csv", "Sparse_parity_train.csv", "Trunk_train.csv",
-    "calcium-spike_train.csv", "mnist_train.csv")
-testSets <- c("Orthant_test.csv", "Sparse_parity_test.csv", "Trunk_test.csv", "calcium-spike_test.csv",
-    "mnist_test.csv")
+trainSets <- c("Orthant_train.csv", "Sparse_parity_train.csv", "Trunk_train.csv", 
+  "calcium-spike_train.csv", "mnist_train.csv")
+testSets <- c("Orthant_test.csv", "Sparse_parity_test.csv", "Trunk_test.csv", "calcium-spike_test.csv", 
+  "mnist_test.csv")
 trainSets <- sapply(trainSets, function(x) paste(path, x, sep = ""))
 testSets <- sapply(testSets, function(x) paste(path, x, sep = ""))
 
@@ -29,8 +29,8 @@ test_skips <- c(FALSE, FALSE, FALSE, FALSE, TRUE)  # skip MNIST data
 #' @param x boolean passed to gc reset
 #'
 getMemUsed <- function(x = FALSE) {
-    tmp <- gc(reset = x)
-    sum(tmp[, ncol(tmp) - 1])
+  tmp <- gc(reset = x)
+  sum(tmp[, ncol(tmp) - 1])
 }
 
 
@@ -45,73 +45,73 @@ getMemUsed <- function(x = FALSE) {
 #' @param cores num. of cores used
 #' @return list of timings and accuracies, where len == numTests
 testDS <- function(trainSet, testSet, numTests, numTrees, cores) {
-    X <- read.csv(trainSet, header = FALSE)
-    Y <- as.numeric(X[, ncol(X)])
-    X <- as.matrix(X[, 1:(ncol(X) - 1)])
-
-    Ymod <- 1 - as.numeric(min(levels(as.factor(Y))))
-    Y <- Y + Ymod
-
-    Xtest <- read.csv(testSet, header = FALSE)
-    Ytest <- as.numeric(Xtest[, ncol(Xtest)]) + Ymod
-    Xtest <- as.matrix(Xtest[, 1:(ncol(Xtest) - 1)])
-
-    # initializing return values
-    ptmtrain <- NA
-    ptmtest <- NA
-    ptmOOB <- NA
-    error <- NA
-    OOBerror <- NA
-    NodeSize <- NA
-    memSize <- NA
-
-    for (i in 1:numTests) {
-        # garbage collection, last two columns gives memory used since last call to gc
-        initMem <- getMemUsed(TRUE)
-
-        ptm <- proc.time()  # start timing
-
-        # create forest
-        forest <- RerF(X, Y, trees = numTrees, min.parent = 1L, max.depth = 0, stratify = TRUE,
-            store.oob = TRUE, num.cores = cores, seed = sample(1:10000, 1))
-
-        ptmtrain[i] <- (proc.time() - ptm)[3]  # time taken
-
-        memSize[i] <- getMemUsed() - initMem  # memory used
-
-        temp_size <- 0
-        for (z in 1:length(forest$trees)) {
-            temp_size <- temp_size + length(forest$trees[[z]]$treeMap)
-        }
-
-        NodeSize[i] <- temp_size/length(forest$trees)
-
-        # time to do the OOB prediction
-        ptm <- proc.time()
-        OOBmat_temp <- OOBPredict(X, forest, num.cores = cores)
-        OOBerror[i] <- mean(OOBmat_temp != Y, na.rm = TRUE)
-        ptmOOB[i] <- (proc.time() - ptm)[3]
-
-        # time to do prediction
-        ptm <- proc.time()
-        error[i] <- mean(Predict(Xtest, forest, num.cores = cores) != Ytest)
-        ptmtest[i] <- (proc.time() - ptm)[3]
+  X <- read.csv(trainSet, header = FALSE)
+  Y <- as.numeric(X[, ncol(X)])
+  X <- as.matrix(X[, 1:(ncol(X) - 1)])
+  
+  Ymod <- 1 - as.numeric(min(levels(as.factor(Y))))
+  Y <- Y + Ymod
+  
+  Xtest <- read.csv(testSet, header = FALSE)
+  Ytest <- as.numeric(Xtest[, ncol(Xtest)]) + Ymod
+  Xtest <- as.matrix(Xtest[, 1:(ncol(Xtest) - 1)])
+  
+  # initializing return values
+  ptmtrain <- NA
+  ptmtest <- NA
+  ptmOOB <- NA
+  error <- NA
+  OOBerror <- NA
+  NodeSize <- NA
+  memSize <- NA
+  
+  for (i in 1:numTests) {
+    # garbage collection, last two columns gives memory used since last call to gc
+    initMem <- getMemUsed(TRUE)
+    
+    ptm <- proc.time()  # start timing
+    
+    # create forest
+    forest <- RerF(X, Y, trees = numTrees, min.parent = 1L, max.depth = 0, stratify = TRUE, 
+      store.oob = TRUE, num.cores = cores, seed = sample(1:10000, 1))
+    
+    ptmtrain[i] <- (proc.time() - ptm)[3]  # time taken
+    
+    memSize[i] <- getMemUsed() - initMem  # memory used
+    
+    temp_size <- 0
+    for (z in 1:length(forest$trees)) {
+      temp_size <- temp_size + length(forest$trees[[z]]$treeMap)
     }
-
-    ret_vals = list(ptmtrain = ptmtrain, ptmtest = ptmtest, ptmOOB = ptmOOB, error = error,
-        OOBerror = OOBerror, NodeSize = NodeSize, memSize = memSize)
-    return(ret_vals)
+    
+    NodeSize[i] <- temp_size/length(forest$trees)
+    
+    # time to do the OOB prediction
+    ptm <- proc.time()
+    OOBmat_temp <- OOBPredict(X, forest, num.cores = cores)
+    OOBerror[i] <- mean(OOBmat_temp != Y, na.rm = TRUE)
+    ptmOOB[i] <- (proc.time() - ptm)[3]
+    
+    # time to do prediction
+    ptm <- proc.time()
+    error[i] <- mean(Predict(Xtest, forest, num.cores = cores) != Ytest)
+    ptmtest[i] <- (proc.time() - ptm)[3]
+  }
+  
+  ret_vals = list(ptmtrain = ptmtrain, ptmtest = ptmtest, ptmOOB = ptmOOB, error = error, 
+    OOBerror = OOBerror, NodeSize = NodeSize, memSize = memSize)
+  return(ret_vals)
 }
 
 
 testDSs <- function(trainSets, testSets, numTests, numTrees, cores, test_skips) {
-    results <- list()
-    for (i in which(!test_skips)) {
-        gc()  # run gc to keep timing pure?
-        ret_vals <- testDS(trainSets[i], testSets[i], numTests[i], numTrees[i], cores[i])
-        results[[i]] <- ret_vals
-    }
-    return(results)
+  results <- list()
+  for (i in which(!test_skips)) {
+    gc()  # run gc to keep timing pure?
+    ret_vals <- testDS(trainSets[i], testSets[i], numTests[i], numTrees[i], cores[i])
+    results[[i]] <- ret_vals
+  }
+  return(results)
 }
 
 # dev mode will let us load in a different version
@@ -143,21 +143,21 @@ print("Finished baseline.")
 
 # test if results are from same distribution
 dist_same <- function(A, B) {
-    wt <- wilcox.test(A, B)$p.value
-    if (wt < 0.001) {
-        return(FALSE)
-    }
-    return(TRUE)
+  wt <- wilcox.test(A, B)$p.value
+  if (wt < 0.001) {
+    return(FALSE)
+  }
+  return(TRUE)
 }
 
-test_that("test different parameters and performance to make sure they are similar",
-    {
-        for (i in which(!test_skips)) {
-            for (param in 1:length(results_candidate[[1]])) {
-                print(names(results_candidate[[i]])[param])
-                expect_true(dist_same(results_candidate[[i]][[param]], results_baseline[[i]][[param]]))
-            }
-        }
-    })
+test_that("test different parameters and performance to make sure they are similar", 
+  {
+    for (i in which(!test_skips)) {
+      for (param in 1:length(results_candidate[[1]])) {
+        print(names(results_candidate[[i]])[param])
+        expect_true(dist_same(results_candidate[[i]][[param]], results_baseline[[i]][[param]]))
+      }
+    }
+  })
 
 
