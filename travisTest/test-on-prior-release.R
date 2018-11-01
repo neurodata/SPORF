6ac0caf342ddca18f0ad1e533d6fc5d437573b61
Ben Falk <falk.ben@gmail.com>
2018-11-01 11:59:41 -0400

    with low # of trees we sometimes get NAs for OOBerror

--- travisTest/test-on-prior-release.R
+++ travisTest/test-on-prior-release.R
@@ -14,9 +14,9 @@ cores <- c(rep(numC, 4), 25)
 numTrees <- c(rep(trees, 4), 100)
 
 path <- "travisTest/"
-trainSets <- c("Orthant_train.csv", "Sparse_parity_train.csv", "Trunk_train.csv", 
+trainSets <- c("Orthant_train.csv", "Sparse_parity_train.csv", "Trunk_train.csv",
     "calcium-spike_train.csv", "mnist_train.csv")
-testSets <- c("Orthant_test.csv", "Sparse_parity_test.csv", "Trunk_test.csv", "calcium-spike_test.csv", 
+testSets <- c("Orthant_test.csv", "Sparse_parity_test.csv", "Trunk_test.csv", "calcium-spike_test.csv",
     "mnist_test.csv")
 trainSets <- sapply(trainSets, function(x) paste(path, x, sep = ""))
 testSets <- sapply(testSets, function(x) paste(path, x, sep = ""))
@@ -25,7 +25,7 @@ testSets <- sapply(testSets, function(x) paste(path, x, sep = ""))
 test_skips <- c(FALSE, FALSE, FALSE, FALSE, TRUE)  # skip MNIST data
 
 
-#' Gets memory used 
+#' Gets memory used
 #' @param x boolean passed to gc reset
 #'
 getMemUsed <- function(x = FALSE) {
@@ -48,14 +48,14 @@ testDS <- function(trainSet, testSet, numTests, numTrees, cores) {
     X <- read.csv(trainSet, header = FALSE)
     Y <- as.numeric(X[, ncol(X)])
     X <- as.matrix(X[, 1:(ncol(X) - 1)])
-    
+
     Ymod <- 1 - as.numeric(min(levels(as.factor(Y))))
     Y <- Y + Ymod
-    
+
     Xtest <- read.csv(testSet, header = FALSE)
     Ytest <- as.numeric(Xtest[, ncol(Xtest)]) + Ymod
     Xtest <- as.matrix(Xtest[, 1:(ncol(Xtest) - 1)])
-    
+
     # initializing return values
     ptmtrain <- NA
     ptmtest <- NA
@@ -64,41 +64,41 @@ testDS <- function(trainSet, testSet, numTests, numTrees, cores) {
     OOBerror <- NA
     NodeSize <- NA
     memSize <- NA
-    
+
     for (i in 1:numTests) {
         # garbage collection, last two columns gives memory used since last call to gc
         initMem <- getMemUsed(TRUE)
-        
+
         ptm <- proc.time()  # start timing
-        
+
         # create forest
-        forest <- RerF(X, Y, trees = numTrees, min.parent = 1L, max.depth = 0, stratify = TRUE, 
+        forest <- RerF(X, Y, trees = numTrees, min.parent = 1L, max.depth = 0, stratify = TRUE,
             store.oob = TRUE, num.cores = cores, seed = sample(1:10000, 1))
-        
+
         ptmtrain[i] <- (proc.time() - ptm)[3]  # time taken
-        
+
         memSize[i] <- getMemUsed() - initMem  # memory used
-        
+
         temp_size <- 0
         for (z in 1:length(forest$trees)) {
             temp_size <- temp_size + length(forest$trees[[z]]$treeMap)
         }
-        
+
         NodeSize[i] <- temp_size/length(forest$trees)
-        
+
         # time to do the OOB prediction
         ptm <- proc.time()
         OOBmat_temp <- OOBPredict(X, forest, num.cores = cores)
-        OOBerror[i] <- mean(OOBmat_temp != Y)
+        OOBerror[i] <- mean(OOBmat_temp != Y, na.rm = TRUE)
         ptmOOB[i] <- (proc.time() - ptm)[3]
-        
+
         # time to do prediction
         ptm <- proc.time()
         error[i] <- mean(Predict(Xtest, forest, num.cores = cores) != Ytest)
         ptmtest[i] <- (proc.time() - ptm)[3]
     }
-    
-    ret_vals = list(ptmtrain = ptmtrain, ptmtest = ptmtest, ptmOOB = ptmOOB, error = error, 
+
+    ret_vals = list(ptmtrain = ptmtrain, ptmtest = ptmtest, ptmOOB = ptmOOB, error = error,
         OOBerror = OOBerror, NodeSize = NodeSize, memSize = memSize)
     return(ret_vals)
 }
@@ -150,7 +150,7 @@ dist_same <- function(A, B) {
     return(TRUE)
 }
 
-test_that("test different parameters and performance to make sure they are similar", 
+test_that("test different parameters and performance to make sure they are similar",
     {
         for (i in which(!test_skips)) {
             for (param in 1:length(results_candidate[[1]])) {
