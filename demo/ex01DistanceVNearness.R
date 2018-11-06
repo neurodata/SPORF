acc74ff975ad7123a26d9575d6b35a9926497ad2
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-11-05 21:57:33 -0500

    Fix errors in check --as-cran closes #85
    Squashed commit of the following:
    
    commit d5a294b82edd44cd1b1ff96af136bfabf4288031
    Author: Jesse Leigh Patsolic <StudioJLP@gmail.com>
    Date:   Mon Nov 5 21:53:02 2018 -0500
    
        fix check --as-cran errors.
    
    commit fefa6dac5b4c06fcfd133352fec33f30d005c978
    Author: Jesse Leigh Patsolic <StudioJLP@gmail.com>
    Date:   Fri Nov 2 16:34:18 2018 -0400
    
        broken, working on --as-cran.

--- demo/ex01DistanceVNearness.R
+++ demo/ex01DistanceVNearness.R
@@ -11,7 +11,7 @@ sizeD <- 1000
 depth <- 8
 k <- 3
 
-# create a sizeD by m synthetic dataset
+# create a size D by m synthetic dataset
 X <- matrix(sort(runif(m * sizeD)), nrow = sizeD, ncol = m)
 AkNN <- matrix(0, nrow = sizeD, ncol = sizeD)
 
@@ -28,32 +28,29 @@ nnzPts <- which(sM$similarityMatrix != 0 & sM$similarityMatrix != 1)
 # create output
 ssd <- data.frame(Distance = AkNN[nnzPts], Nearness = sM$similarityMatrix[nnzPts])
 
-p <- ggplot(aes(x = Nearness, y = Distance), data = ssd) + geom_point() + 
-  labs(title = "Distance vs Similarity of All Points to All Other Points\n10-D Line, n=1000, depth=8, trees=100\n(0 Similarity Omitted)")
+p <- ggplot(aes(x = Nearness, y = Distance), data = ssd) + geom_point() + labs(title = "Distance vs Similarity of All Points to All Other Points\n10-D Line, n=1000, depth=8, trees=100\n(0 Similarity Omitted)")
 p <- p + scale_y_log10()
 p <- p + xlab("Similarity") + ylab("Euclidean Distance")
 print(p)
 
-nnzPts <- which(sM$similarityMatrix[, 499:501] != 0 & sM$similarityMatrix[, 
-  499:501] != 1)
-ssd <- data.frame(Distance = AkNN[, 499:501][nnzPts], Nearness = sM$similarityMatrix[, 
+nnzPts <- which(sM$similarityMatrix[, 499:501] != 0 & sM$similarityMatrix[, 499:501] !=
+  1)
+ssd <- data.frame(Distance = AkNN[, 499:501][nnzPts], Nearness = sM$similarityMatrix[,
   499:501][nnzPts])
 groupLabels <- c(rep("499", sizeD), rep("500", sizeD), rep("501", sizeD))[nnzPts]
 ssd[["Sample"]] <- groupLabels
-p <- ggplot(aes(x = Nearness, y = Distance, color = Sample), data = ssd) + 
-  geom_point() + labs(title = "Distancech vs Similarity of points 499-501 to all other points\n10-D Line, n=1000, depth=8, trees=100\nThree Samples (0 Similarity omitted)") + 
+p <- ggplot(aes(x = Nearness, y = Distance, color = Sample), data = ssd) + geom_point() +
+  labs(title = "Distance vs Similarity of points 499-501 to all other points\n10-D Line, n=1000, depth=8, trees=100\nThree Samples (0 Similarity omitted)") +
   geom_jitter()
 p <- p + scale_y_log10()
 print(p)
 
 nnzPts <- which(sM$similarityMatrix[, 500] != 1)
-ssd <- data.frame(Distance = AkNN[, 500][nnzPts], Nearness = sM$similarityMatrix[, 
+ssd <- data.frame(Distance = AkNN[, 500][nnzPts], Nearness = sM$similarityMatrix[,
   500][nnzPts])
 groupLabels <- (rep("500", sizeD))[nnzPts]
 ssd[["Sample"]] <- groupLabels
-p <- ggplot(aes(x = Nearness, y = Distance), data = ssd) + geom_point() + 
-  labs(title = "Distance vs Similarity of One Observation to All Others\n10-D Line, n=1000, depth=8, trees=100\nThree Samples (0 Similarity Retained)") + 
+p <- ggplot(aes(x = Nearness, y = Distance), data = ssd) + geom_point() + labs(title = "Distance vs Similarity of One Observation to All Others\n10-D Line, n=1000, depth=8, trees=100\nThree Samples (0 Similarity Retained)") +
   geom_jitter()
 p <- p + scale_y_log10()
 print(p)
-
