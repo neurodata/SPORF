5ea6b0e3325cbe2ee7546d476c846916a7cd4e07
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2018-11-20 19:23:44 -0500

    update documentation files.

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,13 +2,13 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 packForestRCPP <- function() {
-  invisible(.Call('_rerf_packForestRCPP', PACKAGE = 'rerf'))
+  invisible(.Call("_rerf_packForestRCPP", PACKAGE = "rerf"))
 }
 
 predictRF <- function(mat, numCores) {
-  .Call('_rerf_predictRF', PACKAGE = 'rerf', mat, numCores)
+  .Call("_rerf_predictRF", PACKAGE = "rerf", mat, numCores)
 }
 
 findSplit <- function(x, y, ndSize, I, maxdI, bv, bs, nzidx, cc) {
-  .Call('_rerf_findSplit', PACKAGE = 'rerf', x, y, ndSize, I, maxdI, bv, bs, nzidx, cc)
+  .Call("_rerf_findSplit", PACKAGE = "rerf", x, y, ndSize, I, maxdI, bv, bs, nzidx, cc)
 }
