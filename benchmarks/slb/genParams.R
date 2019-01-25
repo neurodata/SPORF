1031341679c9740cabdce368f58e0119c5927366
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2019-01-25 00:09:56 -0500

    add commitSHA

--- benchmarks/slb/genParams.R
+++ benchmarks/slb/genParams.R
@@ -3,6 +3,7 @@
 ## This generates the list of parameters for each run
 require(slb)
 
+commitSHA <- "0b999a4382cf0788830d17db68aed5e0eab482e5"
 repository <- "pmlb"
 task <- "classification"
 
@@ -11,7 +12,7 @@ n <- names(slb.load.datasets(repositories = repository, task = task))
 RandMats <- c("RandMatRF", "RandMatBinary", "RandMatContinuous")
 Scale01 <- c(TRUE, FALSE)
 
-out <- as.data.frame(expand.grid(repository = repository, task = task, RandMats = RandMats, Scale01 = Scale01, dataset = n))
+out <- as.data.frame(expand.grid(commitSHA = commitSHA, repository = repository, task = task, RandMats = RandMats, Scale01 = Scale01, dataset = n))
 
 head(out)
 
