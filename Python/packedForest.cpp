fadec740ef7eda3de66808fa11980f02ed9c3ee2
Ben Falk <falk.ben@gmail.com>
2019-01-31 17:29:53 -0500

    fix binding

--- Python/packedForest.cpp
+++ Python/packedForest.cpp
@@ -1,6 +1,7 @@
 #include "../src/packedForest/src/packedForest.h"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <string>
 
@@ -11,23 +12,23 @@ namespace fp
 
 PYBIND11_MODULE(pyfp, m)
 {
-  py::class_<fpForest<double>>(m, "fpForest")
-      .def(py::init<>())
-      .def("setParameter",
-           py::overload_cast<const std::string &, const std::string &>(&fpForest<double>::setParameter),
-           "sets a string parameter")
-      .def("setParameter",
-           py::overload_cast<const std::string &, const int>(&fpForest<double>::setParameter),
-           "sets an int parameter")
-      .def("setParameter",
-           py::overload_cast<const std::string &, const double>(&fpForest<double>::setParameter),
-           "sets a float parameter")
-      .def("printParameters", &fpForest<double>::printParameters)
-      .def("printForestType", &fpForest<double>::printForestType)
-      .def("printForestType", &fpForest<double>::setNumberOfThreads)
-      .def("growForest", &fpForest<double>::growForest)
-      .def("predict", &fpForest<double>::predict)
-      .def("testAccuracy", &fpForest<double>::testAccuracy);
+     py::class_<fpForest<double>>(m, "fpForest")
+         .def(py::init<>())
+         .def("setParameter",
+              py::overload_cast<const std::string &, const std::string &>(&fpForest<double>::setParameter),
+              "sets a string parameter")
+         .def("setParameter",
+              py::overload_cast<const std::string &, const int>(&fpForest<double>::setParameter),
+              "sets an int parameter")
+         .def("setParameter",
+              py::overload_cast<const std::string &, const double>(&fpForest<double>::setParameter),
+              "sets a float parameter")
+         .def("printParameters", &fpForest<double>::printParameters)
+         .def("printForestType", &fpForest<double>::printForestType)
+         .def("setNumberOfThreads", &fpForest<double>::setNumberOfThreads)
+         .def("growForest", &fpForest<double>::growForest)
+         .def("predict", &fpForest<double>::predict)
+         .def("testAccuracy", &fpForest<double>::testAccuracy);
 }
 
 } // namespace fp
