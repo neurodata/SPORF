2d0195c7151b751f68f941d530c3c132cb4acce8
Ben Falk <falk.ben@gmail.com>
2019-02-21 16:03:47 -0500

    adds python binding to packedForest (#174)

---
+++ Python/packedForest.cpp
@@ -0,0 +1,71 @@
+#include "../packedForest/src/packedForest.h"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/iostream.h>
+#include <pybind11/numpy.h>
+
+#include <string>
+
+namespace py = pybind11;
+
+namespace fp
+{
+
+PYBIND11_MODULE(pyfp, m)
+{
+    py::class_<fpForest<double>>(m, "fpForest")
+        .def(py::init<>())
+        .def("setParameter",
+             py::overload_cast<const std::string &, const std::string &>(&fpForest<double>::setParameter),
+             "sets a string parameter")
+        .def("setParameter",
+             py::overload_cast<const std::string &, const int>(&fpForest<double>::setParameter),
+             "sets an int parameter")
+        .def("setParameter",
+             py::overload_cast<const std::string &, const double>(&fpForest<double>::setParameter),
+             "sets a float parameter")
+        .def("printParameters", &fpForest<double>::printParameters,
+             py::call_guard<py::scoped_ostream_redirect,
+                            py::scoped_estream_redirect>())
+        .def("printForestType", &fpForest<double>::printForestType,
+             py::call_guard<py::scoped_ostream_redirect,
+                            py::scoped_estream_redirect>())
+        .def("setNumberOfThreads", &fpForest<double>::setNumberOfThreads)
+
+        .def("_growForestnumpy", [](fpForest<double> &self, py::array_t<double, py::array::c_style | py::array::forcecast> X, py::array_t<int, py::array::c_style | py::array::forcecast> Y, int numObs, int numFeatures) {
+            py::buffer_info Xbuf = X.request();
+            const double *Xptr = (double *)Xbuf.ptr;
+
+            py::buffer_info Ybuf = Y.request();
+            const int *Yptr = (int *)Ybuf.ptr;
+
+            self.growForest(Xptr, Yptr, numObs, numFeatures);
+        })
+
+        .def("_growForest", py::overload_cast<>(&fpForest<double>::growForest))
+        .def("_predict", py::overload_cast<std::vector<double> &>(&fpForest<double>::predict))
+        .def("_predict_numpy", [](fpForest<double> &self, py::array_t<double, py::array::c_style | py::array::forcecast> mat) {
+            py::buffer_info buf = mat.request();
+            double *ptr = (double *)buf.ptr;
+            int numObservations = buf.shape[0];
+            int numFeatures = buf.shape[1];
+
+            std::vector<double> currObs(numFeatures);
+            std::vector<int> predictions(numObservations);
+
+            for (int i = 0; i < numObservations; i++)
+            {
+                for (int j = 0; j < numFeatures; j++)
+                {
+                    currObs[j] = ptr[i * numFeatures + j];
+                }
+                predictions[i] = self.predict(currObs);
+            }
+            return predictions;
+        })
+
+        .def("testAccuracy", &fpForest<double>::testAccuracy);
+}
+
+} // namespace fp
