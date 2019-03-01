cd04aa0bc0b4f1d4a601667022fe3c522df09b3a
Jesse Leigh Patsolic <StudioJLP@gmail.com>
2019-03-01 17:20:34 -0500

    add check for square-ness

--- R-Project/R/RunPredictSim.R
+++ R-Project/R/RunPredictSim.R
@@ -102,7 +102,10 @@ RunPredictSim <-
       }
     }
 
-    ### Make output matrix symmetric
-    predictions[upper.tri(predictions)] <- t(predictions)[upper.tri(predictions)]
+    ### If square, make output matrix symmetric
+    ### TODO: this could be made into a class similar to base::dist
+    if (dim(predictions)[1] == dim(predictions)[2]) {
+      predictions[upper.tri(predictions)] <- t(predictions)[upper.tri(predictions)]
+    }
     return(predictions)
   }
