4d64c5fa6094c0543da6f15ffaf4f51d77b4b7ad
James D. Browne Jr <jbrowne6@jhu.edu>
2019-03-15 13:44:28 -0400

    added the files required for R bindings. (#182)
    * added the files required for R bindings.
    
    * added seed parameter.  Seed isn't working, but parameter can be set.
    
    * removed seed check until seeding is fixed.
    
    * update install instructions and travis.
    
    * Add proper roxygen comment to export
    forestPackingRConversion to NAMESPACE file.
    Update documentation and tests.
    
    * update Makevars flags.
    
    * update docs
    
    * update docs
    
    * Update install instructions
    update fpRerF example and forest-types
    
    * add check for correct forestType
    
    * update test and yaml
    
    * fix travis command error.
    
    * update tests for R-Dev.
    
    * should fix travis???
    
    * add flag to stop test on failure.
    
    * update test-predict
    
    * :construction: omp flags for mac os with clang.
    
    * :construction: this possibly breaks -ffast-math on systems other than JLP's
    
    * update travis to run `R CMD build` and `R CMD check`
    
    * update README install for Mac OS.
    
    * update to README
    
    * set warnings_are_errors <- FALSE
    this will allow merging of the Rbindings with openMP support.
    
    * update travis.
    
    * Revert "update test-predict"
    
    This reverts commit 6ff5e8861fc54fb0755f390b473031a958e0b15e.
    
    * trying to revert to a state where TravisCI can install.
    
    * remove  from Makevars.

---
+++ R-Project/R/fpPredict.R
@@ -0,0 +1,33 @@
+#' Packs a forest and saves modified forest to disk for use by PackPredict function
+#'
+#' Efficiently packs a forest trained with the RF option.  Two intermediate data structures are written to disk, forestPackTempFile.csv and traversalPackTempFile.csv.  The size of these data structures is proportional to a trained forest and training data respectively.  Both data structures are removed at the end of the operation.  The resulting forest is saved as forest.out.  The size of this file is similar to the size of the trained forest.
+#'
+#' @param forest forest data structure returned from fpRerF function.
+#' @param X an n by d numeric matrix (preferable) or data frame used to train the forest. 
+#'
+#'
+#' @export
+#'
+#' @examples
+#'
+#' library(rerf)
+#' forest <- fpRerF(as.matrix(iris[, 1:4]), as.numeric(iris[[5L]])-1)
+#' predictions <- fpPredict(forest, as.matrix(iris[, 1:4]))
+#
+fpPredict <-
+	function(forest=NULL,X=NULL){
+		if(class(forest)!="Rcpp_forestPackingRConversion"){
+			stop("forest is not of correct type.")
+		}
+
+		if(is.null(X)){
+			stop("no observations to predict.")
+		}
+
+		X <- as.matrix(X)
+		if(class(X[1,1])=="integer"){
+			storage.mode(X) <- "numeric"
+		}
+
+		forest$predict(X)
+	}
---
+++ R-Project/R/zzz.R
@@ -0,0 +1,4 @@
+#' Rcpp module: forestPackingRConversion
+#' @name forestPackingRConversion
+#' @export
+loadModule("forestPackingRConversion_mod", TRUE)
