8ae31ae9a5630c6c392226870cde70057ad9bc84
Ben Falk <falk.ben@gmail.com>
2019-03-21 15:54:18 -0400

    passes for RerF forest w/ processingNodeBin

--- packedForest/test/fpTests/processingNodeBinTest.h
+++ packedForest/test/fpTests/processingNodeBinTest.h
@@ -5,27 +5,57 @@
 
 TEST(testProcessingNodeBin, testMtryMult)
 {
+  int mtry = 2;
+
   fpSingleton::getSingleton().setParameter("CSVFileName", "../res/iris.csv");
   fpSingleton::getSingleton().setParameter("columnWithY", 4);
-  fpSingleton::getSingleton().setParameter("numTreesInForest", 10);
-  fpSingleton::getSingleton().setParameter("minParent", 1);
-  fpSingleton::getSingleton().setParameter("numCores", 1);
-  fpSingleton::getSingleton().setParameter("mtry", 2);
-  // fpSingleton::getSingleton().setParameter("forestType", "binnedBaseRerF");
+  fpSingleton::getSingleton().setParameter("mtry", mtry);
+  fpSingleton::getSingleton().loadData();
+  fpSingleton::getSingleton().setDataDependentParameters();
 
-  randomNumberRerFMWC randNum;
-  randNum.initialize(-1661580697);
   int treeNum = 1;
   int nodeNum = 0;
   int depth = 0;
+
+  randomNumberRerFMWC randNum;
+  std::random_device rd;
+  int seed = rd();
+  randNum.initialize(seed);
+
   processingNodeBin<double, std::vector<int>> pNodeBin(treeNum, nodeNum, depth, randNum);
 
-  // for (int setMtryMult = 1; setMtryMult < 10; ++setMtryMult)
-  // {
-    int setMtryMult = 1;
+  for (double setMtryMult = 1; setMtryMult < 4; setMtryMult+=0.5)
+  {
     fpSingleton::getSingleton().setParameter("mtryMult", setMtryMult);
 
     std::vector<std::vector<int>> f2Try;
     pNodeBin.calcMtryForNodeTest(f2Try);
-  // }
+
+    EXPECT_EQ(f2Try.size(), mtry);
+
+    std::vector<int> features_sampled;
+    for (auto i : f2Try)
+    {
+      for (auto ii : i)
+      {
+        features_sampled.push_back(ii);
+        EXPECT_GE(ii, 0);
+        EXPECT_LT(ii, fpSingleton::getSingleton().returnNumFeatures());
+      }
+    }
+    EXPECT_EQ(features_sampled.size(), (int)(setMtryMult * mtry));
+
+    //printing
+    // std::cout << "mtryMult: " << fpSingleton::getSingleton().returnMtryMult() << "\n";
+    // int mtry = 0;
+    // for (auto i : f2Try)
+    // {
+    //   std::cout << mtry++ << ": ";
+    //   for (auto ii : i)
+    //   {
+    //     std::cout << ii << ",";
+    //   }
+    //   std::cout << "\n";
+    // }
+  }
 }
