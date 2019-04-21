9e1c89ba804793c2108c47379cab0bb07efe7853
Meghana <meghana.madhyastha@gmail.com>
2019-04-21 18:55:26 -0500

    Fixed minor things

--- packedForest/src/forestTypes/unsupervisedForests/urerf/fpURerFBase.h
+++ packedForest/src/forestTypes/unsupervisedForests/urerf/fpURerFBase.h
@@ -79,8 +79,6 @@ namespace fp {
 					trees[i].growTree();
 					trees[i].updateSimMat(simMat, pairMat);
 				}
-				createSparseMat();
-				//printSparseMat();
 			}
 
 			inline void checkParameters(){
@@ -142,8 +140,6 @@ namespace fp {
 					totalLeafNodes += trees[i].returnNumLeafNodes();
 					totalLeafDepth += trees[i].returnLeafDepthSum();
 				}
-
-				std::cout << "precision@10: " << computePrecision(10)<<"\n";
 			}
 
 
