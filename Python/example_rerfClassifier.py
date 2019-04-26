b6751813ad8ec54440789481af3f9c4d2600215d
Ben Falk <falk.ben@gmail.com>
2019-04-26 16:56:10 -0400

    adds example for rerfClassifier class

---
+++ Python/example_rerfClassifier.py
@@ -0,0 +1,18 @@
+from rerfClassifier import rerfClassifier
+from sklearn.datasets import make_classification
+
+X, y = make_classification(
+    n_samples=1000,
+    n_features=4,
+    n_informative=2,
+    n_redundant=0,
+    random_state=0,
+    shuffle=False,
+)
+
+clf = rerfClassifier(n_estimators=100, max_depth=2, random_state=0)
+clf.fit(X, y)
+
+print(clf.predict([0, 0, 0, 0]))
+print(clf.predict_proba([0, 0, 0, 0]))
+print(clf)
