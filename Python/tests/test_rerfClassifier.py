6bf749020192f65d2735da126dabdcc77d0de486
Ben Falk <falk.ben@gmail.com>
2019-06-11 10:14:01 -0400

    this test will fail until we fix OOB in python

--- Python/tests/test_rerfClassifier.py
+++ Python/tests/test_rerfClassifier.py
@@ -193,3 +193,16 @@ def test_iris_perfect_train(projection_matrix):
 
         # want to make sure that if any are not giving 100% acc, it's only 1 obs that's wrong
         assert min(y_train_acc_list) >= (1 - 1 / len(iris_full.target))
+
+
+def test_iris_OOB(projection_matrix="RerF"):
+    iris_full = datasets.load_iris()
+    y_train_acc_list = []
+    clf = rerfClassifier(
+        n_estimators=50, projection_matrix=projection_matrix, oob_score=True
+    )
+
+    clf.fit(iris_full.data, iris_full.target)
+
+    assert clf.oob_score_ < 0.2
+
