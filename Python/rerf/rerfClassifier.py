66383fdf3f48e5bc1a5c3f0734e3f63f2b608ac0
Ben Falk <falk.ben@gmail.com>
2019-06-11 11:33:38 -0400

    fix the OOB fix (#287)
    * believe you missed an updateOOB func call
    
    * this test will fail until we fix OOB in python
    
    * Update test_rerfClassifier.py
    
    * adds back check for oob and set forest unbinned

--- Python/rerf/rerfClassifier.py
+++ Python/rerf/rerfClassifier.py
@@ -208,10 +208,16 @@ class rerfClassifier(BaseEstimator, ClassifierMixin):
         self.forest_ = pyfp.fpForest()
 
         if self.projection_matrix == "Base":
-            forestType = "binnedBase"
+            if self.oob_score:
+                forestType = "rfBase"
+            else:
+                forestType = "binnedBase"
             self.method_to_use_ = None
         elif self.projection_matrix == "RerF":
-            forestType = "binnedBaseTern"
+            if self.oob_score:
+                forestType = "rerf"
+            else:
+                forestType = "binnedBaseTern"
             self.method_to_use_ = 1
         elif self.projection_matrix == "S-RerF":
             forestType = "binnedBaseTern"  # this should change
@@ -281,7 +287,6 @@ class rerfClassifier(BaseEstimator, ClassifierMixin):
         else:
             self.random_state_ = self.random_state
         self.forest_.setParameter("seed", self.random_state_)
-
 
         # need to set mtry here (using max_features and calc num_features):
         if self.max_features in ("auto", "sqrt"):
