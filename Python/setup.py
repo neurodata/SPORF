99cdf54e26b5ae9f647b76b3d166d034abf836a8
Ben Falk <falk.ben@gmail.com>
2019-07-23 22:35:19 -0400

    include isn't mac specific

--- Python/setup.py
+++ Python/setup.py
@@ -101,6 +101,8 @@ def cpp_flag(compiler):
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
 
+    extra_include = "inc"
+
     c_opts = {
         "msvc": ["/EHsc", "/openmp"],
         "unix": [
@@ -110,6 +112,7 @@ class BuildExt(build_ext):
             "-O3",
             "-DNDEBUG",
             "-ffast-math",
+            "-I{}".format(extra_include),
         ],
     }
 
@@ -123,13 +126,10 @@ class BuildExt(build_ext):
         ompbase = subprocess.run(["brew", "--prefix", "libomp"], stdout=subprocess.PIPE)
         omploc = ompbase.stdout.decode("utf-8").strip()
 
-        extra_include = "inc"
-
         c_opts["unix"] += [
             "-lomp",
             "-I{}/include".format(omploc),
             "-L{}/lib".format(omploc),
-            "-I{}".format(extra_include),
         ]
 
     def build_extensions(self):
